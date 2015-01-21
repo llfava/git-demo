19dbd52800a5bad6273ea4624fc6f640bf1f6392
Laura Fava <laura.fava@outlook.com>
2015-01-20 21:09:26 -0800

    Adding scraper_test.py. Currently can crawl search results for bars in SF.  No scraping yet

---
+++ scraper_test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import splinter
+from splinter import Browser
+import time
+import random
+
+
+
+def main():
+
+    browser = Browser()
+
+    url = 'http://www.yelp.com/search?find_desc=bars&find_loc=San+Francisco%2C+CA&ns=1'
+    browser.visit(url)
+
+    ##TODO: Need to scrape here
+    ##Want to get Business name, yelp url associated with business, number of reviews, aggregate rating, address
+    ##Add to file
+    arrow_link = browser.find_link_by_partial_href('/search?find_desc=bars&find_loc=San+Francisco%2C+CA&start=')
+    arrow_link.click()
+
+    ##TODO: Need to scrape here
+
+    for a in range(2, 10):
+        print a
+        delay = random.uniform(3,5)
+        print delay
+        time.sleep(delay)
+
+        arrow_link = browser.find_link_by_partial_href('/search?find_desc=bars&find_loc=San+Francisco%2C+CA&start=' + str(a))
+        arrow_link.click()
+
+        ##TODO: Need to scrape here
+
+    ##TODO: Go to bar_yelp website and scrape review level data: user id, user location, review date, review text, 
+    ## review rating
+    ##Add to file
+
+    return 0
+
+if __name__ == "__main__":
+    main()
