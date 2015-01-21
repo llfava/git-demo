d27439934e82c88d091b8e47653b50be961b36ae
Laura Fava <laura.fava@outlook.com>
2015-01-21 12:32:28 -0800

    Updated scraper_test.py to pull bar attributes for multiple pages.  Still prints to screen

--- scraper_test.py
+++ scraper_test.py
@@ -5,11 +5,18 @@ from splinter import Browser
 import time
 import random
 
-def get_business__url(browser):
+def get_business_url(browser):
     a_list = browser.find_by_tag('a')
     for a in a_list:
         if a['class'] == 'biz-name':
             print a['href']
+    return 0
+
+def get_business_name(browser):
+    a_list = browser.find_by_tag('a')
+    for a in a_list:
+        if a['class'] == 'biz-name':
+            print a.text
     return 0
 
 def get_business_aggregate_rating(browser):
@@ -64,51 +71,53 @@ def get_business_category(browser):
             print span.text
     return 0
 
-def main():
-
-    browser = Browser()
-
-    url = 'http://www.yelp.com/search?find_desc=bars&find_loc=San+Francisco%2C+CA&ns=1'
-    browser.visit(url)
-
-
-    ###get business attributes
-    get_business__url(browser)
+def get_business_attributes(browser):
+    get_business_url(browser)
+    get_business_name(browser)
     get_business_aggregate_rating(browser)
     get_num_reviews(browser)
     get_neighborhood(browser)
     get_business_address(browser)
     get_business_category(browser)
-        
+    return 0
 
-    ##TODO: Need to scrape here
-    ##Want to get Business name, yelp url associated with business, number of reviews, aggregate rating, address
-    ##Add to file
+def main():
 
-#    browser.quit()
-    return
+    browser = Browser()
 
+    #Go to first page of search results for bars+SF on yelp
+    url = 'http://www.yelp.com/search?find_desc=bars&find_loc=San+Francisco%2C+CA&ns=1'
+    browser.visit(url)
 
+    get_business_attributes(browser)
+
+    #Go to second page of search results
     arrow_link = browser.find_link_by_partial_href('/search?find_desc=bars&find_loc=San+Francisco%2C+CA&start=')
     arrow_link.click()
 
-    ##TODO: Need to scrape here
+    time.sleep(3) ## Must have time delay here or doesn't work!!!
 
-    for a in range(2, 10):
+    get_business_attributes(browser)
+
+
+    for a in range(2, 5):
         print a
-        delay = random.uniform(3,5)
-        print delay
-        time.sleep(delay)
 
         arrow_link = browser.find_link_by_partial_href('/search?find_desc=bars&find_loc=San+Francisco%2C+CA&start=' + str(a))
         arrow_link.click()
 
-        ##TODO: Need to scrape here
+        delay = random.uniform(3,5)
+        time.sleep(delay)
+
+        get_business_attributes(browser)
+
 
     ##TODO: Go to bar_yelp website and scrape review level data: user id, user location, review date, review text, 
     ## review rating
     ##Add to file
 
+
+#    browser.quit()
     return 0
 
 if __name__ == "__main__":
