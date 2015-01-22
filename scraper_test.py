5495d562b99a4f9dcc71b79a6f1aa9ae746bcf06
Laura Fava <laura.fava@outlook.com>
2015-01-21 17:42:15 -0800

    Updated scraper_test.py to scrape first page of search results for bars in SF and save results to json file called bars.txt

--- scraper_test.py
+++ scraper_test.py
@@ -2,84 +2,112 @@
 
 import splinter
 from splinter import Browser
+import json
 import time
 import random
 
+
 def get_business_url(browser):
+    business_urls = []
     a_list = browser.find_by_tag('a')
     for a in a_list:
         if a['class'] == 'biz-name':
-            print a['href']
-    return 0
+            business_urls.append(str(a['href']))
+    return business_urls
 
 def get_business_name(browser):
+    business_names = []
     a_list = browser.find_by_tag('a')
     for a in a_list:
         if a['class'] == 'biz-name':
-            print a.text
-    return 0
+            business_names.append(a.text)
+    return business_names
+
 
 def get_business_aggregate_rating(browser):
+    business_ratings = []
     i_list = browser.find_by_tag('i')
     for i in i_list:
         if i['class'] == 'star-img stars_5':
-            print i['title']
+            business_ratings.append(str(i['title']))
         if i['class'] == 'star-img stars_5_half':
-            print i['title']
+            business_ratings.append(str(i['title']))
         if i['class'] == 'star-img stars_4':
-            print i['title']
+            business_ratings.append(str(i['title']))
         if i['class'] == 'star-img stars_4_half':
-            print i['title']
+            business_ratings.append(str(i['title']))
         if i['class'] == 'star-img stars_3':
-            print i['title']
+            business_ratings.append(str(i['title']))
         if i['class'] == 'star-img stars_3_half':
-            print i['title']
+            business_ratings.append(str(i['title']))
         if i['class'] == 'star-img stars_2':
-            print i['title']
+            business_ratings.append(str(i['title']))
         if i['class'] == 'star-img stars_2_half':
-            print i['title']
+            business_ratings.append(str(i['title']))
         if i['class'] == 'star-img stars_1':
-            print i['title']
+            business_ratings.append(str(i['title']))
         if i['class'] == 'star-img stars_1_half':
-            print i['title']
-    return 0
+            business_ratings.append(str(i['title']))
+    return business_ratings
 
 def get_num_reviews(browser):
+    num_reviews = []
     span_list = browser.find_by_tag('span')
     for span in span_list:
         if span['class'] == 'review-count rating-qualifier':
-            print span.text
-    return 0
+            num_reviews.append(str(span.text))
+    return num_reviews
 
 def get_neighborhood(browser):
+    neighborhoods = []
     span_list = browser.find_by_tag('span')
     for span in span_list:
         if span['class'] == 'neighborhood-str-list':
-            print span.text
-    return 0
+            neighborhoods.append(str(span.text))
+    return neighborhoods
 
 def get_business_address(browser):
+    business_addresses = []
     address_list = browser.find_by_tag('address')
     for address in address_list:
-        print address.text
-    return 0
+        business_addresses.append(str(address.text))
+    return business_addresses
 
 def get_business_category(browser):
+    categories = []
     span_list = browser.find_by_tag('span')
     for span in span_list:
         if span['class'] == 'category-str-list':
-            print span.text
-    return 0
+            categories.append(str(span.text))
+    return categories
 
 def get_business_attributes(browser):
-    get_business_url(browser)
-    get_business_name(browser)
-    get_business_aggregate_rating(browser)
-    get_num_reviews(browser)
-    get_neighborhood(browser)
-    get_business_address(browser)
-    get_business_category(browser)
-    return 0
+    business_urls = get_business_url(browser)
+    business_names = get_business_name(browser)
+    business_ratings = get_business_aggregate_rating(browser)
+    num_reviews = get_num_reviews(browser)
+    neighborhoods = get_neighborhood(browser)
+    business_addresses = get_business_address(browser)
+    categories = get_business_category(browser)
+
+    return(business_urls,
+           business_names,
+           business_ratings,
+           num_reviews,
+           neighborhoods,
+           business_addresses,
+           categories)
+
+def make_bar_dict(business_urls, business_names, business_ratings, num_reviews, neighborhoods, business_addresses, categories):
+    keys = business_urls
+    values = []
+
+    for j in range(len(business_urls)):                                                                                                              
+        values.append([business_names[j], business_ratings[j], num_reviews[j], neighborhoods[j], business_addresses[j], categories[j]])
+
+    bar_dict = dict(zip(keys, values))
+    return(bar_dict)
+
 
 def main():
 
@@ -89,17 +117,22 @@ def main():
     url = 'http://www.yelp.com/search?find_desc=bars&find_loc=San+Francisco%2C+CA&ns=1'
     browser.visit(url)
 
-    get_business_attributes(browser)
+    business_urls, business_names, business_ratings, num_reviews, neighborhoods, business_addresses, categories = get_business_attributes(browser)
+    bar_dict = make_bar_dict(business_urls, business_names, business_ratings, num_reviews, neighborhoods, business_addresses, categories)
+    #print json.dumps(bar_dict, sort_keys=True, indent=2)
+    with open('bars.txt', 'w') as outfile:
+        json.dump(bar_dict, outfile, indent=2)
 
+    return
     #Go to second page of search results
     arrow_link = browser.find_link_by_partial_href('/search?find_desc=bars&find_loc=San+Francisco%2C+CA&start=')
     arrow_link.click()
 
     time.sleep(3) ## Must have time delay here or doesn't work!!!
 
-    get_business_attributes(browser)
+    business_urls = get_business_attributes(browser)    
 
-
+    #Go to third and subsequent pages of search results
     for a in range(2, 5):
         print a
 
